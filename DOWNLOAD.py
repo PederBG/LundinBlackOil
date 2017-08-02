0e36fd52d20c1a827a13e71444e02c2f8f3bc309
Peder Grundvold <pedergrbr@gmail.com>
2017-08-02 11:40:49 +0100

    Commented download script & add satellite tracking

--- DOWNLOAD.py
+++ DOWNLOAD.py
@@ -1,35 +1,46 @@
 #!/bin/sh
 ''''cd /home/lundinbl/public_html/peder/ && exec /home/lundinbl/Python27_v2/Python-2.7.13/python  -- "$0" # '''
+
 import time
 import os
 import sys
+from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
+import zipfile
+import snappy
+from snappy import ProductIO, GPF, ProgressMonitor
+import urllib2
+import xml.etree.ElementTree as ET
+import cv2
+import math
+import shutil
+# ----------------------------------------------------------------
 sys.path.append("/home/lundinbl/.local/lib/python2.7/site-packages/snappy")
 sys.path.append("/home/lundinbl/.local/lib/python2.7/site-packages/jpy")
 
+# WRITING TO LOG FILE
 class Tee(object):
     def __init__(self, *streams):
         self.streams = streams
+
     def write(self, obj):
         for s in self.streams:
             s.write(obj)
             s.flush()
-    def flush(self) :
+
+    def flush(self):
         for s in self.streams:
             s.flush()
 
 logFile = open('sentinelDownload.log', 'w')
-
 tee = Tee(sys.stdout, logFile)
-
 sys.stdout = tee
 sys.stderr = tee
-
+# ----------------------------------------------------------------
 
 # connect to the API
-from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
-
-# api = SentinelAPI('PederBG', 'Copernicus', 'https://scihub.copernicus.eu/dhus')
-api = SentinelAPI('PederBG', 'Copernicus','https://colhub.met.no/#/home')  # Use this if the area of interest is within Norway
+# api = SentinelAPI('PederBG', 'Copernicus', 'https://scihub.copernicus.eu/dhus') # For areas outside Norway
+api = SentinelAPI('PederBG', 'Copernicus',
+                  'https://colhub.met.no/#/home')  # Use this if the area of interest is within Norway
 
 # search by polygon, time, and SciHub query keywords
 footprint = geojson_to_wkt(read_geojson('smallBarentsSea.geojson'))
@@ -41,46 +52,46 @@ products = api.query(footprint,
                      producttype='GRD',
                      sensoroperationalmode='IW'
                      )
-                     
+
 if len(products) == 0:
-    print("No files found")
+    print("No files found at date: " + date)
     quit()
 for i in range(len(products)):
     print(products[products.keys()[i]])
 
 products_df = api.to_dataframe(products)
 
+# FINDING SMALLEST FILE
 smallestFile = None
 tempSize = 9999999999
 for i in range(0, len(products)):
     if (api.get_product_odata(products_df.index[i])["size"] < tempSize):
         smallestFile = products_df.index[i]
         tempSize = api.get_product_odata(products_df.index[i])["size"]
+# ----------------------------------------------------------------
 
+# SETTING MAX SIZE AND GETTING PRODUCT INFO
 maxSize = 500000000  # Set the max size for files to download (in bytes)
 if (tempSize < maxSize):
     api.download(smallestFile)
     smallestName = api.get_product_odata(smallestFile)["title"]
-    smallestDate = api.get_product_odata(smallestFile)["date"].strftime("%d-%m-%Y_%H:%M")
+    smallestDate = api.get_product_odata(smallestFile)["date"].strftime("%d-%m-%Y_%H-%M") # ":" cause error in windowsOS
     smallestLink = api.get_product_odata(smallestFile)["url"]
     print("Downloading " + smallestName + ", Size: " + str(tempSize) + " bytes.")
 else:
     print("No file small enough to download")
+# ----------------------------------------------------------------
 
-### Unzipping downloaded file
+
+# UNZIPPING DOWNLOADED FILE
 print("Unzipping product")
-import zipfile
+
 zip_ref = zipfile.ZipFile(smallestName + '.zip')
 zip_ref.extractall()
 zip_ref.close()
+# -------------------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------- #
 ###################### SNAP PROCESSING ##########################
-
-import snappy
-from snappy import ProductIO
-from snappy import GPF
-from snappy import ProgressMonitor
 
 HashMap = snappy.jpy.get_type('java.util.HashMap')
 
@@ -89,8 +100,7 @@ product = ProductIO.readProduct(smallestName + '.SAFE/manifest.safe')
 print("Processing...")
 print(product)
 
-
-### CALIBRATION
+# CALIBRATION
 print("Calibrating product...")
 params0 = HashMap()
 params0.put('outputSigmaBand', True)
@@ -99,8 +109,9 @@ params0.put('selectedPolarisations', 'VV')
 params0.put('outputImageScaleInDb', False)
 
 calib = GPF.createProduct("Calibration", params0, product)
+# ----------------------------------------------------------------
 
-### MAKING SUBSET
+# MAKING SUBSET
 print("Making subset...")
 WKTReader = snappy.jpy.get_type('com.vividsolutions.jts.io.WKTReader')
 wkt = geojson_to_wkt(read_geojson('veryBigBarentsSea.geojson'))  # Lundin landing site in Barents Sea
@@ -115,8 +126,9 @@ ProductIO.writeProduct(target_0, "temp", 'BEAM-DIMAP')
 
 # Reading in new product
 productSUB = ProductIO.readProduct('temp.dim')
+# ----------------------------------------------------------------
 
-### SPECKLE FILTER
+# SPECKLE FILTER
 print("Speckle filtering...")
 params1 = HashMap()
 params1.put('sourceBands', 'Sigma0_' + 'VV')
@@ -125,19 +137,21 @@ ProductIO.writeProduct(target_1, "temp2", 'BEAM-DIMAP')
 
 # Reading in new product
 productFILT = ProductIO.readProduct('temp2.dim')
+# ----------------------------------------------------------------
 
-### TERRAIN CORRECTION
+# TERRAIN CORRECTION
 print("Range Doppler Terrain Correction...")
 params2 = HashMap()
 params2.put('demName', 'ACE30')
 params2.put('nodataValueAtSea', False)
-target_2 = GPF.createProduct('Terrain-Correction', params2, productFILT )
+target_2 = GPF.createProduct('Terrain-Correction', params2, productFILT)
 ProductIO.writeProduct(target_1, "temp3", 'BEAM-DIMAP')
 
 # Reading in new product
 productTER = ProductIO.readProduct('temp3.dim')
+# ----------------------------------------------------------------
 
-### MAKING IMAGE
+# MAKING IMAGE
 print("Generating image...")
 JPY = snappy.jpy
 imageIO = JPY.get_type('javax.imageio.ImageIO')
@@ -152,19 +166,19 @@ name = File('temp.png')
 print("...")
 imageIO.write(image, 'PNG', name)
 print("Image created")
-
+# -------------------------------------------------------------------------------------------
 
 ################### GETTING WIND INFORMATION ####################
 # CONNECT TO YR.NO
 print("Connecting to yr.no...")
-import urllib2
 
-GRID = '72.2922222_21.8055556' # LANDING SITE GRID (center of square)
+GRID = '72.2922222_21.8055556'  # LANDING SITE GRID (center of square)
 splitGrid = GRID.split('_')
 response = urllib2.urlopen('https://www.yr.no/place/Hav/' + GRID + '/varsel.xml')
 html = response.read()  # Observations from Goliat platform
+# ----------------------------------------------------------------
 
-import xml.etree.ElementTree as ET
+# PARSING XML
 tree = ET.ElementTree(ET.fromstring(html))
 root = tree.getroot()
 
@@ -174,77 +188,87 @@ obsName = root[0][3].get('geobaseid')
 obsDir = root[5][0][0][2].get('name')
 
 print("Wind: ", obsSpeed, " mps, direction: ", obsDeg, "degrees.")
-
+# -------------------------------------------------------------------------------------------
 
 ################### WRITING ON THE IMAGE ###################
-#TODO (bruke logaritmisk skala eller noe...)?
-print("Writing weather to image...")
-import cv2
-import math
-img = cv2.imread('temp.png')  # reading in generated image
+# TODO -> FIX BAD/COMPLICATED CODE HERE: (use logarithmic scale?)
+
+# READING IN GENERATED IMAGE
+print("Writing weather info to image...")
+img = cv2.imread('temp.png')
 obsDeg = - math.radians(obsDeg)  # negative radians to turn arrow the correct way
 
 imgHeight = img.shape[0]
 imgWidth = img.shape[1]
 print("Image height: ", imgHeight, ", image width: ", imgWidth)
+# ----------------------------------------------------------------
 
 # COMPUTING IMAGE HEIGHT AND WIDTH TO SCALE TEXT/ARROW
 if (imgHeight > imgWidth):
-    p = imgHeight / 600
+    p = imgHeight / 750
+    lineSpeed = obsSpeed/1.5
 else:
     p = (img.shape[0] + img.shape[1]) / 1500
+    lineSpeed = obsSpeed
+
 print("Computed P-ratio: ", p)
+# ----------------------------------------------------------------
 
 # LINE LENGTH CONTROL
-lineSpeed = obsSpeed
 lineSize = 2
 if (lineSpeed > 25):
     lineSize = 4
     lineSpeed = lineSpeed / 8
 elif (lineSpeed > 10):
     lineSize = 3
-    lineSpeed = lineSpeed/3
+    lineSpeed = lineSpeed / 3
 
 startX = 50
 startY = 100
-x = 10*(lineSpeed * math.sin(obsDeg)) + startX
-y = 10*(lineSpeed * math.cos(obsDeg)) + startY
+x = 10 * (lineSpeed * math.sin(obsDeg)) + startX
+y = 10 * (lineSpeed * math.cos(obsDeg)) + startY
 
 print("x: ", x, ", y: ", y)
+# ----------------------------------------------------------------
 
 # MORE LINE LENGTH CONTROL
 if (x < 0):
-    startX += -(x - 10*p)
+    startX += -(x - 10 * p)
     x += -(x - 10)
 if (y < 0):
-    startY += -(y - 10*p)
+    startY += -(y - 10 * p)
     y += -(y - 10)
 
 print("startX: ", startX, ", startY: ", startY)
 print("X: ", x, ", Y: ", y)
+# ----------------------------------------------------------------
 
-
-draw = cv2.arrowedLine(img, (int(startX)*p, int(startY)*p), (int(x)*p,int(y)*p), (0,0,255), lineSize*p)
+# DRAWING ON IMAGE
+draw = cv2.arrowedLine(img, (int(startX) * p, int(startY) * p), (int(x) * p, int(y) * p), (0, 0, 255), lineSize * p)
 font = cv2.FONT_HERSHEY_COMPLEX
-cv2.putText(img,('Wind data from grid: ' + splitGrid[0]+'N, ' + splitGrid[1]+'E'),(10*p,20*p), font, 0.3*p,(0,0,255),p)
-cv2.putText(img,(str(obsSpeed)+" mps, from "+obsDir),(10*p,40*p),font,0.3*p,(0,0,255),p)
+cv2.putText(img, ('Wind data from grid: ' + splitGrid[0] + 'N, ' + splitGrid[1] + 'E'), (10 * p, 20 * p), font, 0.3 * p,
+            (0, 0, 255), p)
+cv2.putText(img, (str(obsSpeed) + " mps, from " + obsDir), (10 * p, 40 * p), font, 0.3 * p, (0, 0, 255), p)
 
-saveName = 'sentinel_images/sentinel-image(a)_' + smallestDate + '.png' # file name
+saveName = 'sentinel_images/sentinel-image(a)2_' + smallestDate + '.png'  # file name
 cv2.imwrite(saveName, img)
 print("Wind vector created and added to image!")
+# -------------------------------------------------------------------------------------------
 
 ################# RESIZE AND MAKEING THUMBNAIL ################
 resizeInput = cv2.imread(saveName)
 height, width = img.shape[:2]
-resized = cv2.resize(resizeInput,(int(0.05*width), int(0.05*height)), interpolation = cv2.INTER_CUBIC)
-cv2.imwrite("sentinel_images/sentinel-image(a)_" + smallestDate + "(R).png", resized)
+resized = cv2.resize(resizeInput, (int(0.05 * width), int(0.05 * height)), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite("sentinel_images/sentinel-image(a)2_" + smallestDate + "(R).png", resized)
 print("Thumbnail image created!")
+# -------------------------------------------------------------------------------------------
 
 ################# ADDING PRODUCT DOWNLOAD LINK TO TEXT-FILE ################
 print("Appending to download_links txt-file...")
 txt_file = open("product_download_links.txt", "a")
 txt_file.write(saveName + ' ' + str(smallestLink) + "\n")
 txt_file.close()
+# -------------------------------------------------------------------------------------------
 
 #################### DELETING USED FILES AND MAKING ZIP-FILE #################### (only works on linux OS)
 # deletes the used files
@@ -254,16 +278,13 @@ os.remove('temp.png')
 os.remove('temp2.dim')
 os.remove('temp3.dim')
 
-
-import shutil
 shutil.rmtree(smallestName + '.SAFE')
 shutil.rmtree('temp.data')
 shutil.rmtree('temp2.data')
 shutil.rmtree('temp3.data')
+# -------------------------------------------------------------------------------------------
 
-
-
-# Comressing directory to enable download
+# COMPRESSING TO ENABLE DIRECT DOWNLOAD
 zf = zipfile.ZipFile("sentinel_images.zip", "w")
 for dirname, subdirs, files in os.walk("sentinel_images"):
     zf.write(dirname)
